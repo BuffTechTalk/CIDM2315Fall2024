01643aaccea2b5962678b4208064fabe517fadec
Cheng Zhang <79437133+BuffTechTalk@users.noreply.github.com>
2024-09-09 14:04:56 -0500

    Homework2

---
+++ CarlZhang-Homework2.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework2;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int b = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("The value of b: "+ b);
+    }
+}
